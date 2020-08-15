fa5f085e951766d13b71c767e015f2ff8759082c
Charles Amat <charles@infalliblecode.com>
2020-08-15 12:31:30 -0400

    Added a domain class to represent Identities

--- grails-app/conf/BootStrap.groovy
+++ grails-app/conf/BootStrap.groovy
@@ -1,6 +1,12 @@
+import com.force5solutions.demo.*
+
 class BootStrap {
 
     def init = { servletContext ->
+        new Person(firstName: 'Charles', lastName: 'Amat').save()
+        new Person(firstName: 'Nathan', lastName: 'Harris').save()
+        new Person(firstName: 'Robert', lastName: 'Evelyn').save()
+        new Person(firstName: 'James', lastName: 'Evelyn').save()
     }
     def destroy = {
     }
---
+++ grails-app/domain/com/force5solutions/demo/Person.groovy
@@ -0,0 +1,10 @@
+package com.force5solutions.demo
+
+class Person {
+    String firstName
+    String lastName
+    FacialIdentifier facialIdentifier
+
+    static constraints = {
+    }
+}
